abaa8bed7be7e01afb2671c40f2ae9afa8b50627
Xavier Paez <xavipaez2000@hotmail.com>
2022-06-27 14:55:51 -0500

    Configuraciones Log4j

--- src/main/java/com/uce/edu/demo/ProyectoU2XpApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2XpApplication.java
@@ -1,20 +1,32 @@
 package com.uce.edu.demo;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sun.tools.javac.Main;
+
 @SpringBootApplication
 public class ProyectoU2XpApplication implements CommandLineRunner {
-
+	
+	private static Logger logger=Logger.getLogger(Main.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2XpApplication.class, args);
+		
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		logger.debug("LOG DEBUG");
+		logger.info("LOG INFO");
+		logger.warn("LOG WARN");
+		logger.error("LOG ERROR");
+		logger.fatal("LOG FATAL");
+
 	}
 
 }
