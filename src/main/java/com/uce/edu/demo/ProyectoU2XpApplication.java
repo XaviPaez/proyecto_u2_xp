336d89a416ab5a75ee43fa18358eb88ea02f3e6c
Xavier Paez <xavipaez2000@hotmail.com>
2022-06-27 20:19:45 -0500

    EJERCICIO CON JDBC A POSTGRESQL

--- src/main/java/com/uce/edu/demo/ProyectoU2XpApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2XpApplication.java
@@ -1,16 +1,23 @@
 package com.uce.edu.demo;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.apache.log4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.sun.tools.javac.Main;
+import com.uce.edu.demo.service.IPersonaJdbcService;
+import com.uce.edu.demo.to.Persona;
 
 @SpringBootApplication
 public class ProyectoU2XpApplication implements CommandLineRunner {
 	
-	private static Logger logger=Logger.getLogger(Main.class);
+	private static final Logger LOG= LoggerFactory.getLogger(ProyectoU2XpApplication.class);
+	@Autowired
+	private IPersonaJdbcService iPersonaJdbcService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2XpApplication.class, args);
@@ -20,13 +27,23 @@ public class ProyectoU2XpApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
-		logger.debug("LOG DEBUG");
-		logger.info("LOG INFO");
-		logger.warn("LOG WARN");
-		logger.error("LOG ERROR");
-		logger.fatal("LOG FATAL");
-
+		/*Persona persona= new Persona();
+		persona.setId(3);
+		persona.setNombre("Xavier");
+		persona.setApellido("Paez");
+		//Insertar
+		//this.iPersonaJdbcService.guardar(persona);
+		Persona persona2=new Persona();
+		persona2.setId(3);
+		persona2.setNombre("Xavier");
+		persona2.setApellido("Martinez");
+		//Actualizar
+		//this.iPersonaJdbcService.actualizar(persona2);
+		
+		this.iPersonaJdbcService.eliminar(3);*/
+		
+		
+		LOG.info(this.iPersonaJdbcService.buscarPorId(1).toString());
 	}
 
 }
