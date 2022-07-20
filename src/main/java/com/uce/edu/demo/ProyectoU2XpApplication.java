aa99845f6d51720f952e8d4593efe95324f1ad6a
Xavier Paez <xavipaez2000@hotmail.com>
2022-07-19 20:09:39 -0500

    TALLER 24: Mapeo JPA OneToOne

--- src/main/java/com/uce/edu/demo/ProyectoU2XpApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2XpApplication.java
@@ -1,5 +1,6 @@
 package com.uce.edu.demo;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -11,12 +12,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uce.edu.demo.repository.modelo.Ciudadano;
+import com.uce.edu.demo.repository.modelo.Empleado;
 import com.uce.edu.demo.repository.modelo.Estudiante;
 import com.uce.edu.demo.repository.modelo.EstudianteContadorSemestre;
 import com.uce.edu.demo.repository.modelo.EstudianteSencillo;
 import com.uce.edu.demo.repository.modelo.Persona;
 import com.uce.edu.demo.repository.modelo.PersonaContadorGenero;
 import com.uce.edu.demo.repository.modelo.PersonaSencilla;
+import com.uce.edu.demo.service.ICiudadanoService;
 import com.uce.edu.demo.service.IEstudianteJpaService;
 import com.uce.edu.demo.service.IPersonaJpaService;
 
@@ -30,6 +34,9 @@ public class ProyectoU2XpApplication implements CommandLineRunner {
 
 	@Autowired
 	private IEstudianteJpaService estudianteJpaService;
+	
+	@Autowired
+	private ICiudadanoService ciudadanoService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2XpApplication.class, args);
@@ -38,23 +45,28 @@ public class ProyectoU2XpApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		LOG.info("\n");
-		LOG.info("Estudiante sencillo");
-
-		List<EstudianteSencillo> listaEstudianteSencillos = this.estudianteJpaService
-				.buscarPorSemestreSencillo("Octavo");
-		for (EstudianteSencillo estuItem : listaEstudianteSencillos) {
-			LOG.info("Estudiante Sencillo: " + estuItem);
-		}
-		LOG.info("\n");
-
-		LOG.info("Estudiante sencillo con agrupamiento");
-		List<EstudianteContadorSemestre> miListaContadorSemestres = this.estudianteJpaService
-				.consultarCantidadPorSemestre();
-		for (EstudianteContadorSemestre item : miListaContadorSemestres) {
-			LOG.info("Estudiante: " + item);
-		}
-
+		Ciudadano ciu1 = new Ciudadano();
+		ciu1.setNombre("Joss");
+		ciu1.setApellido("Riera");
+		
+		Empleado empl1 = new Empleado();
+		empl1.setCodigoIess("123");
+		empl1.setSalario(new BigDecimal(200));
+		empl1.setCiudadano(ciu1);
+		
+		ciu1.setEmpleado(empl1);
+		this.ciudadanoService.insertar(ciu1);
+		
+		Ciudadano ciu2= new Ciudadano();
+		ciu2.setNombre("Joss");
+		ciu2.setApellido("Riera");
+		
+		Empleado empl2 = new Empleado();
+		empl2.setCodigoIess("123");
+		empl2.setSalario(new BigDecimal(200));
+		empl2.setCiudadano(ciu1);
+		
+		ciu2.setEmpleado(empl2);
+	
 	}
 }
